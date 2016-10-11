3547a4f826722248efc0ae88345353efdda06ebb
katinuta <katinuts@gmail.com>
2016-10-11 22:43:30 +0200

    Some

--- dao/src/main/java/by/htp/teploukhava/testing/dao/daointerface/AnswerDAO.java
+++ dao/src/main/java/by/htp/teploukhava/testing/dao/daointerface/AnswerDAO.java
@@ -12,7 +12,6 @@ public interface AnswerDAO extends AbstractDAO<Answer> {
 	public List<Answer> findQuestionAnswers(int questionId) throws DAOException;
 
 	public int updateAnswer(Answer answer, String content, Boolean rightAnswer) throws DAOException;
-
-
+//Some
 
 }
