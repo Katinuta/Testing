6106b1d6cb43c4fb8edbdde29fc022bacf7553f7
katinuta <katinuts@gmail.com>
2017-01-03 16:54:12 +0200

    add spring authentication

--- web/src/main/java/by/htp/teploukhava/testing/filter/AuthenticationUserDetails.java
+++ web/src/main/java/by/htp/teploukhava/testing/filter/AuthenticationUserDetails.java
@@ -2,6 +2,9 @@ package by.htp.teploukhava.testing.filter;
 
 import by.htp.teploukhava.testing.dao.impl.UserDAOImpl;
 import by.htp.teploukhava.testing.entities.User;
+import by.htp.teploukhava.testing.serviceimpl.UserService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,31 +12,47 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Admin on 20.11.16.
  */
 @Service
-public class MyUserDetails implements UserDetailsService {
+public class AuthenticationUserDetails implements UserDetailsService {
     @Autowired
-    private UserDAOImpl userDAOImpl;
+    private UserService userService;
 
-    public MyUserDetails(UserDAOImpl userDAOImpl){
-        this.userDAOImpl=userDAOImpl;
+    private static final Logger logger= LogManager.getLogger(AuthenticationUserDetails.class);
+
+    public AuthenticationUserDetails(){}
+
+    public AuthenticationUserDetails(UserDAOImpl userDAOImpl){
+        this.userService=userService;
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        User user=userDAOImpl.findUserByLogin(login);
-       List<GrantedAuthority> authority=null;
+        User user=userService.findUserByLogin(login);
+        logger.info(user.toString());
+        System.out.println(user.toString());
+       List<GrantedAuthority> authority=new ArrayList<>();
+        System.out.println(user.isAccess());
         if(user.isAccess()){
             authority.add(new SimpleGrantedAuthority("ROLE_ADMIN")) ;
         }else{
             authority.add(new SimpleGrantedAuthority("ROLE_USER"));
         }
-         UserDetails userDetails=new org.springframework.security.core.userdetails.User(user.getLogin(),user.getPassword(),authority);
+         UserDetails userDetails=new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), true,
+                 true, true, true,  authority);
+
+        System.out.println(userDetails.getUsername()+" "+ userDetails.getPassword());
+        logger.info(user.getLogin()+ "   " +user.getPassword());
+        System.out.println(user.getLogin()+ "   " +user.getPassword());
         return userDetails;
     }
 }
+
--- web/src/main/java/by/htp/teploukhava/testing/filter/SuccessHandler.java
+++ web/src/main/java/by/htp/teploukhava/testing/filter/SuccessHandler.java
@@ -1,19 +1,48 @@
 package by.htp.teploukhava.testing.filter;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by Admin on 21.11.16.
  */
-public class CustomSuccessHandler implements AuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+@Component
+public class SuccessHandler implements AuthenticationSuccessHandler {
 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        /*Set target URL to redirect*/
+        String targetUrl = determineTargetUrl(authentication);
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(Authentication authentication) {
+        Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (authorities.contains("ROLE_ADMIN")) {
+            return "/admin/subjects/1";
+        } else if (authorities.contains("ROLE_USER")) {
+            return "/client/subjects/1";
+        } else {
+           return "/accessDenied";
+        }
+    }
+    public RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
     }
 }
